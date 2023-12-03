5eb0ae09fcaf9764b4615bfe1fc26e7e30777fee
Donal Hanna <donal@the-plot.com>
2023-12-03 12:07:03 +0000

    Update bedside.py

--- bedside.py
+++ bedside.py
@@ -6,7 +6,18 @@ import time
 import requests
 from rndWebContent import RndWebContent
 from PIL import Image
+# for some reaon this isn't working:
+# from rpi_backlight import Backlight
+import logging
+logging.basicConfig(filename="/home/zog/bedside/log.txt",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger(__name__)
 
+logger.setLevel(logging.INFO)
+logger.info("starting...")
+
+import os
 
 window = ctk.CTk()
 window.attributes('-fullscreen', True)
@@ -20,6 +31,7 @@ landingLight = HueLight("Landing", window)
 diningRoomLight = HueLight("Dining Room", window)
 
 clockLbl = ctk.CTkLabel(window, width=150, height=50, bg_color=("transparent"), fg_color=("transparent"), font=("Callibri", 40))
+#bl = Backlight()
 weatherLbl = ctk.CTkLabel(window, width=350, height=40, bg_color=("transparent"), fg_color=("transparent"), font=("Callibri", 30))
 webLbl = ctk.CTkLabel(window, width=750, height=350, justify="left", wraplength=700, bg_color=("transparent"), fg_color=("transparent"), font=("Callibri", 20))
 screenTitle = ctk.CTkLabel(window, width=250, height=35, justify="center", bg_color=("transparent"), fg_color=("transparent"), font=("Callibri", 25))
@@ -35,18 +47,54 @@ def placeLightControlBtns():
     diningRoomLight.btn.place(x=640, y=410)
 
 def time():
-    string = strftime('%d/%m    %H:%M %p')
-    clockLbl.configure(text=string)
+    #logger.info("in da time...")
+    dateTimeDisplayString = strftime('%d/%m    %H:%M %p')
+    timeCheckString = strftime("%H:%M")
+    #logger.info(timeCheckString)
+    if timeCheckString == "22:00":
+        #logger.info("it's 10pm")
+        #bl.brightness = 20
+        os.system("rpi-backlight -b 20")
+    elif timeCheckString == "23:00":
+        #logger.info("it's 11pm")
+        #bl.brighness = 5
+        os.system("rpi-backlight -b 5")
+    elif timeCheckString == "08:00":
+        #bl.brighness = 100
+        os.system("rpi-backlight -b 100")
+    #else:
+    #    logger.info("none of the above")
+    clockLbl.configure(text=dateTimeDisplayString)
     clockLbl.after(30000, time)
+    #clockLbl.after(10000, time)
+
 
 def weatherCheck():
-   apiKey="yourKeyHere"
+   apiKey="goes here"
    apiURL="https://api.openweathermap.org/data/2.5/weather?lat=52.33&lon=-0.179&units=metric&appid="+apiKey
    response = requests.get(apiURL)
    data = response.json()
    weatherString = str(data['main']['temp'])+'\xb0C, '+ data['weather'][0]['main']
    weatherLbl.configure(text=weatherString)
    weatherLbl.after(10800000, weatherCheck)
+
+#not using but leaving for now
+#def screenShot():
+#    options = Options()
+#    #options.add_argument('headless')
+#    options.add_argument('window-size=2048,1000')
+#    driver = webdriver.Chrome(options=options)  # Optional argument, if not specified will search path.
+#    driver.get('https://bbc.co.uk/news')
+#    driver.save_screenshot('my_screenshot.png')
+#    driver.quit()
+#    img = Image.open('my_screenshot.png')
+#    # this has to keep the same ratio as the window size - it doesn't atm:
+#    beebArea = (400,80,2000,1000)
+#    cropped = img.crop(beebArea)
+#    resized = cropped.resize((750,341))
+#    labelImg = ctk.CTkImage(resized, size=(750,300))
+#    webLbl.place(x=15,y=60)mainLabelContent = RndWebContent(webLbl)
+#    webLbl.configure(image=labelImg)
 
 def main():
     ctk.set_appearance_mode("dark")
